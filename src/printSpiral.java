cf18de140d171944350c204b2087be3523a8e97f
Paolo <paoloinocencion@gmail.com>
2017-10-03 18:56:02 -0400

    final

---
+++ src/printSpiral.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class printSpiral {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int n = sc.nextInt();
+
+        int n = 9;
+
+        for (int i = 0; i < n; i++) { //increment line
+            for (int j = 0; j < n; j++) { // increment spaces
+                if (j > i)
+                    System.out.print(n - i); // prints succeeding digits
+                else
+                    System.out.print(n - j);
+            }
+
+            for (int j = n - 2; j >= 0; j--) { // increment spaces
+                if (j < i) //
+                    System.out.print(n - j); // prints succeeding digits
+                else
+                    System.out.print(n - i);
+            }
+
+            System.out.println();
+
+        }
+        for (int i = n - 2; i + 1 > 0; i--) { //increment line
+            for (int j = 0; j < n; j++) { // increment spaces
+                if (j > i)
+                    System.out.print(n - i); // prints succeeding digits
+                else
+                    System.out.print(n - j);
+            }
+
+            for (int j = n - 2; j >= 0; j--) { // increment spaces
+                if (j < i) //
+                    System.out.print(n - j); // prints succeeding digits
+                else
+                    System.out.print(n - i);
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
